ed5ba7806d5d00f2999a5e5eeaefdf9d7f9260dc
Titouan Van Belle <titouan.vanbelle@gmail.com>
2018-02-12 19:09:25 +0100

    Added dependency to Xcode 9.0 to xctesthtmlreport

--- Formula/xctesthtmlreport.rb
+++ Formula/xctesthtmlreport.rb
@@ -4,6 +4,8 @@ class Xchtmlreport < Formula
   url "https://github.com/TitouanVanBelle/XCTestHTMLReport/raw/develop/xchtmlreport-1.4.0.tar.gz"
   sha256 "e52f45fdbd070298ef3b4e92bc38ba05354684235767cdb01447295d71ade97e"
 
+  depends_on :xcode => "9.0"
+
   def install
     system "xcodebuild -workspace XCTestHTMLReport.xcworkspace -scheme XCTestHTMLReport build"
   end
